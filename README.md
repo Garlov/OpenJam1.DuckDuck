6f4557cf7f663bd438ae8e29daf9be5662504f91
Garlov <oe.nordli@gmail.com>
2017-10-06 15:21:54 +0200

    Boilerplate

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 [![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC--4-blue.svg)](https://creativecommons.org/licenses/by-nc/4.0/)
 # OpenJam1.ChromaConflict
 Small arcade/strategy game made for Open Jam starting October 6th 2017
+
+![Phaser+ES6+Webpack](https://raw.githubusercontent.com/lean/phaser-es6-webpack/master/assets/images/phaser-es6-webpack.jpg)
+Phaser-Webpack boilerplate can be found at: https://github.com/lean/phaser-es6-webpack
---
+++ src/states/Game.js
@@ -0,0 +1,26 @@
+/* globals __DEV__ */
+import Phaser from 'phaser'
+
+export default class extends Phaser.State {
+  init () {}
+  preload () {}
+
+  create () {
+    const bannerText = 'Chroma Conflict'
+    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
+    banner.font = 'Bangers'
+    banner.padding.set(10, 16)
+    banner.fontSize = 40
+    banner.fill = '#77BFA3'
+    banner.smoothed = false
+    banner.anchor.setTo(0.5)
+
+    this.game.add.existing(this.mushroom)
+  }
+
+  render () {
+    if (__DEV__) {
+      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+    }
+  }
+}
